bf5c2f274a8e2fc3fbe89b0560dac645f2562c89
Arda Ordu <ardaordu03@gmail.com>
2022-09-28 14:27:01 +0100

    Complete median task

--- median.py
+++ median.py
@@ -1,5 +1,7 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+from statistics import median
+
 
 while True:
     try:
@@ -9,4 +11,21 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
 print(numbers)
+
+result = median(numbers)
+print(result)
+
+# Doing it without median package.
+# numbers.sort()
+
+# if (len(numbers) % 2 == 1):
+#     median_index = (len(numbers) + 1) / 2
+#     result = numbers[int(median_index - 1)]  
+
+# else:
+#     median_index = len(numbers) / 2
+#     result = (numbers[int(median_index)] + numbers[int(median_index) - 1]) / 2
+
+# print(result)
